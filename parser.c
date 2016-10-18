34692db19c3e0ed556258dfa733ed33d39f987d7
Pedro Pereira <grey.pedrinho@gmail.com>
2016-10-18 17:21:02 -0200

    Added stable, added error, started parser.c

---
+++ parser.c
@@ -0,0 +1,95 @@
+#include "error.h"
+#include "stable.h"
+#include "buffer.h"
+#include "parser.h"
+#include "optable.h"
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+
+void checkOperand(char *line, SymbolTable alias_table, int i, Operator word, int operandNo) {
+    
+}
+
+int parse(const char *s, SymbolTable alias_table, Instruction **instr, const char **errptr) {
+    char *line, *label;
+    Buffer *analizer;
+    Operator *word;
+    InsertionResult result;
+    int i, j, len;
+
+    line = strdup(s);
+    len = strlen(line);
+    for(i = 0; i < len && line[i] <= 32; i++); // Takes off all preceding spaces
+    analizer = buffer_create();]
+    if(line[i] == '*' || i >= len) {
+        // Line is composed only by comment
+        return 1;
+    }
+    for(; line[i] > 32 && line[i] != ';' && line[i] != '*'; i++) {
+        buffer_push_back(analizer, line[i]);
+    }
+    word = optable_find(analizer->data);
+    if(word == NULL) {
+        // First word is label
+        if(analizer[0] == '_' || (analizer[0] >= 'a' && analizer[0] <= 'z') ||
+        (analizer[0] >= 'A' && analizer[0] <= 'Z')) {
+            // Label is valid
+            for(j = 1; j < analizer->i; j++) {
+                if(!(analizer[j] == '_' || (analizer[j] >= 'a' && analizer[j] <= 'z') ||
+                (analizer[j] >= 'A' && analizer[j] <= 'Z') || (analizer[j] >= '0' && analizer[j] <= '9'))) {
+                    // Label is invalid
+                    //i - analizer->i + j
+                    *errptr = s[i - analizer->i + j];
+                    set_error_msg("Invalid label");
+                    return 0;
+                }
+            }
+            // Label is still valid -- insert it into stable
+            result = stable_insert(alias_table, analizer->data);
+            if(result.new == 0) {
+                // Label was already declared
+                // i - 1
+                *errptr = s[i - 1];
+                set_error_msg("Label %s was already declared\n", analizer->data);
+                return 0;
+            }
+            label = strdup(analizer->data);
+        }
+        else {
+            // Label is invalid
+            // i - buffer->topo + 1
+            *errptr = s[i - analizer->i + 1];
+            set_error_msg("Invalid label\n");
+            return 0;
+        }
+        // Label registered. Scans next word
+        for(i; i < len && line[i] <= 32; i++); // Takes off all spaces
+        if(i >= len || line[i] == '*') {
+            // Line was only a label
+            // i
+            *errptr = s[i];
+            set_error_msg("Expected operator\n");
+            return 0;
+        }
+        buffer_reset(analizer);
+        for(; i < len && line[i] > 32 && line[i] != ';' && line[i] != '*'; i++) {
+            buffer_push_back(analizer, line[i]);
+        }
+        word = optable_find(analizer->data);
+        if(word == NULL) {
+            // Word after label is not an Operator
+            // i - analizer->i + 1
+            *errptr = s[i - analizer->i + 1];
+            set_error_msg("Expected operator\n");
+            return 0;
+        }
+        (*instr)->label = label;
+    }
+    else {
+        (*instr)->label = "n/a";
+    }
+    (*instr)->op = word;
+
+}
---
+++ stable.c
@@ -0,0 +1,163 @@
+#include "stable.h"
+#include <stdio.h>
+#include "error.h"
+#include <string.h>
+
+// The symbol table.
+typedef struct {
+	char *key;
+	EntryData data;
+} Entry;
+
+struct stable_s {
+	Entry *Table;
+	int topo;
+	int tmax;
+};
+
+int buscaBin (SymbolTable table, const char *key) {
+	int ini = 0, fim = table->topo, meio, comp;
+	meio = (fim+ini)/2;
+	while (ini <= fim) {
+		comp = strcmp(key, table->Table[meio].key);
+		if (comp == 0){
+			return meio;
+		}
+		if (comp < 0){
+			fim = meio-1;
+			meio = (fim+ini)/2;
+		}
+		else {
+			ini = meio+1;
+			meio = (fim+ini)/2;
+		}
+	}
+	meio = (fim+ini)/2;
+	comp = strcmp(key, table->Table[meio].key);
+	if (meio == 0 && comp < 0){
+		return meio;
+	}
+	if (comp > 0){
+		return meio+1;
+	}
+	if (meio < table->topo && comp < 0){
+		return meio+1;
+	}	
+	return meio;
+}
+
+void realloc_table (SymbolTable table) {
+	Entry *New;
+	int i, j;
+	New = emalloc(sizeof(Entry) * table->tmax * 2);
+	for (i = 0; i < 2*table->tmax; i++){
+		New[i].key = emalloc(256*sizeof(char));
+		for (j = 0; j < 255; j++)
+			New[i].key[j] = 0;
+	}
+	for (i = 0; i < table->tmax; i++){
+		New[i] = table->Table[i];
+	}
+	table->tmax *= 2;
+	for (; i < table->tmax; i++){
+		strcpy(New[i].key, "~~\0");
+	}
+	free(table->Table);
+	table->Table = New;
+}
+/*
+  Return a new symbol table.
+*/
+SymbolTable stable_create() {
+	SymbolTable tabela = emalloc(sizeof(struct stable_s));
+	tabela->topo = 0;
+	tabela->tmax = 20;
+	tabela->Table = emalloc(20 * sizeof(Entry));
+	for (int i = 0; i < tabela->tmax; i++){
+		tabela->Table[i].key = emalloc(256*sizeof(char));
+		strcpy(tabela->Table[i].key, "~~\0");
+	}
+	return tabela;
+}
+
+/*
+  Destroy a given symbol table.
+*/
+void stable_destroy(SymbolTable table) {
+	for (int i = 0; i < table->tmax; i++)
+		free(table->Table[i].key);
+	free(table->Table);
+	free(table);
+}
+
+/*
+  Insert a new entry on the symbol table given its key.
+
+  If there is already an entry with the given key, then a struct
+  InsertionResult is returned with new == 0 and data pointing to the
+  data associated with the entry. Otherwise, a struct is returned with
+  new != 0 and data pointing to the data field of the new entry.
+
+  If there is not enough space on the table, or if there is a memory
+  allocation error, then crashes with an error message.
+*/
+InsertionResult stable_insert(SymbolTable table, const char *key) {
+	InsertionResult *res = emalloc(sizeof(InsertionResult));
+	int pos = buscaBin(table, key);
+	char *mem;
+	if (strcmp(table->Table[pos].key, key)){
+		if (table->topo == (table->tmax)-1){
+			realloc_table(table);
+		}
+		for (int i = table->topo; i > pos; i--){
+			table->Table[i] = table->Table[i-1];
+		}
+		mem = malloc(256*sizeof(char));
+		strcpy(mem, key);
+		table->Table[pos].key = mem;
+		res->new = 1;
+		res->data = &(table->Table[pos].data);
+		(table->topo)++;
+	}
+	else {
+		res->data = &(table->Table[pos].data);
+		res->new = 0;
+	}
+	return *res;
+}
+
+/*
+  Find the data associated with a given key.
+
+  Given a key, returns a pointer to the data associated with it, or a
+  NULL pointer if the key is not found.
+*/
+EntryData *stable_find(SymbolTable table, const char *key) {
+	int pos = buscaBin(table, key);
+	if (pos == -1)
+		return NULL;
+	EntryData *res;
+	res = &(table->Table[pos].data);
+	return res;
+}
+
+/*
+  Visit each entry on the table.
+
+  The visit function is called on each entry, with pointers to its key
+  and data. If the visit function returns zero, then the iteration
+  stops.
+
+  Returns zero if the iteration was stopped by the visit function,
+  nonzero otherwise.
+*/
+int stable_visit(SymbolTable table,
+                 int (*visit)(const char *key, EntryData *data)){
+	int i = 0;
+	while (i < table->topo && visit(table->Table[i].key, &(table->Table[i].data))){
+		i++;
+	}
+	if (i == table->topo)
+		return 1;
+	return 0;
+}
